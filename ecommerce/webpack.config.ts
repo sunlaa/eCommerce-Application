542639d22591b84a71d7c8b02fab13ba68b2be40
katyastan <134015338+katyastan@users.noreply.github.com>
2024-05-02 19:53:56 +0300

    feat: add webpack, TS, ESLint, Prettier, Husky, Jest

---
+++ ecommerce/webpack.config.ts
@@ -0,0 +1,56 @@
+import * as path from 'path';
+import { merge } from 'webpack-merge';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import { type Configuration } from 'webpack';
+
+import * as ProdConfig from './webpack.prod.config';
+import * as DevConfig from './webpack.dev.config';
+
+const baseConfig: Configuration = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+      //   favicon: path.resolve(__dirname, './src/img/favicon.ico'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+interface ModeEnv {
+  mode: 'prod' | 'dev';
+}
+
+export default ({ mode }: ModeEnv): Configuration => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? ProdConfig.default : DevConfig.default;
+
+  return merge(baseConfig, envConfig);
+};
---
+++ ecommerce/webpack.prod.config.ts
@@ -0,0 +1,7 @@
+import { type Configuration } from 'webpack';
+
+const Config: Configuration = {
+  mode: 'production',
+};
+
+export default Config;
