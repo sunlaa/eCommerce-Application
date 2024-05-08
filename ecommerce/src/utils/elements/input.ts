64d0dd4e63d5a8498899a75a977e7997af21f6dc
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-05-08 14:16:09 +0200

    EA-35 feat: create Input component

---
+++ ecommerce/src/utils/elements/input.ts
@@ -0,0 +1,32 @@
+import { ParamsOmitTag } from '../types/types';
+import BaseElement from './basic_element';
+
+export default class Input extends BaseElement<HTMLInputElement> {
+  constructor(params: ParamsOmitTag<HTMLInputElement>) {
+    super({ tag: 'input', autocomplete: 'off', ...params });
+  }
+
+  get value() {
+    return this.element.value;
+  }
+
+  set value(text: string) {
+    this.element.value = text;
+  }
+
+  focus() {
+    this.element.focus();
+  }
+
+  clear() {
+    this.element.value = '';
+  }
+
+  off() {
+    this.element.disabled = true;
+  }
+
+  on() {
+    this.element.disabled = false;
+  }
+}
