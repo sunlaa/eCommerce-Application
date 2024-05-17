83cfc8554434e8006fea6bf03dbff4b86bdbd2d2
katyastan <134015338+katyastan@users.noreply.github.com>
2024-05-17 18:59:09 +0300

    EA-60 refactor: change section element

--- ecommerce/src/utils/elements/section.ts
+++ ecommerce/src/utils/elements/section.ts
@@ -1,8 +1,8 @@
 import { ParamsOmitTag } from '../types_variables/types';
 import BaseElement from './basic_element';
 
-export default class Section extends BaseElement<HTMLButtonElement> {
-  constructor(params: ParamsOmitTag<HTMLButtonElement>) {
+export default class Section extends BaseElement {
+  constructor(params: ParamsOmitTag) {
     super({ tag: 'section', ...params });
   }
 }
