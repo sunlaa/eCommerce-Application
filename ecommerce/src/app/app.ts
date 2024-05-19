7c8f9a941d1b5a9944f4990580973a417e24688f
Uladzislava Santalava <119731695+sunlaa@users.noreply.github.com>
2024-05-19 23:51:28 +0200

    EA-67 feat: style not found page (#33)

--- ecommerce/src/app/app.ts
+++ ecommerce/src/app/app.ts
@@ -2,7 +2,6 @@ import './general.sass';
 import LoginFormEngine from '@/components/authentication/login/login_engine';
 import RegFormEngine from '@/components/authentication/registration/registration_engine';
 import MainPage from '@/components/main_page/main';
-import Page404 from '@/components/not_found_page/not_found';
 import BaseElement from '@/utils/elements/basic_element';
 import Router from '@/utils/services/routing';
 import { Routes } from '@/utils/types_variables/types';
@@ -22,7 +21,7 @@ export default class App {
 
   run() {
     // Для изменения контента внутри main можно изменять строку здесь, в соответсвии с путями ниже
-    Router.navigateTo('404');
+    Router.navigateTo('main');
   }
 
   createRoutes(): Routes[] {
@@ -46,13 +45,6 @@ export default class App {
         callback: () => {
           this.container.removeChildren();
           this.container.append(new LoginFormEngine().loginFormEngineStart());
-        },
-      },
-      {
-        path: '404',
-        callback: () => {
-          this.container.removeChildren();
-          this.container.append(new Page404());
         },
       },
     ];
