1fe9e6862bb35731fcb8e09041e33275e7ca98ca
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-05-26 11:35:36 +0200

    EA-77 feat: continue to implement breadcrumb nav

--- ecommerce/src/components/catalog_product_page/catalog_header/breadcrumb_navigation.ts
+++ ecommerce/src/components/catalog_product_page/catalog_header/breadcrumb_navigation.ts
@@ -2,7 +2,14 @@ import Anchor from '@/utils/elements/anchor';
 import BaseElement from '@/utils/elements/basic_element';
 import { CLASS_NAMES } from '@/utils/types_variables/variables';
 
+type Path = {
+  name: string;
+  link: BaseElement;
+};
+
 export default class Breadcrumb extends BaseElement {
+  currentPath: Path[] = [];
+
   separator = new BaseElement({ tag: 'span', content: ' / ' });
 
   constructor() {
@@ -14,6 +21,32 @@ export default class Breadcrumb extends BaseElement {
 
   addLink(name: string) {
     const separator = this.separator.element.cloneNode(true) as HTMLElement;
-    this.appendChildren(separator, new BaseElement({ styles: { display: 'inline' }, content: name }));
+    const link = this.createLink(name);
+    this.currentPath.push({ name, link });
+    this.appendChildren(separator, link);
   }
+
+  removeAfter(name: string) {
+    let index = NaN;
+    this.currentPath.forEach((path, i) => {
+      if (!isNaN(index)) {
+        console.log('here');
+        const link = path.link.element;
+        const separator = link.previousElementSibling as HTMLElement;
+        link.remove();
+        separator.remove();
+      }
+      path.name === name ? (index = i) : null;
+      console.log(path.name, name, index);
+    });
+    // this.currentPath = this.currentPath.slice(0, index);
+  }
+
+  createLink = (name: string) => {
+    const element = new BaseElement({ styles: { display: 'inline' }, content: name });
+    element.addListener('click', () => {
+      this.removeAfter(name);
+    });
+    return element;
+  };
 }
