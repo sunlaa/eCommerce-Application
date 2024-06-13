6b57aef5a99207273fc1c9986ffc9c0bf2fbedb2
katyastan <134015338+katyastan@users.noreply.github.com>
2024-06-13 23:13:38 +0300

    EA-99 feat: add const to variables

--- ecommerce/src/components/about_page/about_page_ui.ts
+++ ecommerce/src/components/about_page/about_page_ui.ts
@@ -1,5 +1,5 @@
 import BaseElement from '@/utils/elements/basic_element';
-import { TEXT_CONTENT, CLASS_NAMES } from '@/utils/types_variables/variables';
+import { TEXT_CONTENT, CLASS_NAMES, teamMembers } from '@/utils/types_variables/variables';
 import Section from '@/utils/elements/section';
 import Paragraph from '@/utils/elements/paragraph';
 import './about_page.sass';
@@ -16,54 +16,6 @@ export default class AboutPageUi extends BaseElement {
     sectionAbout.appendChildren(backImg);
 
     const aboutTitle = new Paragraph('We are', [CLASS_NAMES.about.aboutTitle]);
-
-    const teamMembers = [
-      {
-        name: 'Lada Santalava',
-        role: 'Frontend Developer',
-        image: 'https://github.com/sunlaa/commerce-images/blob/main/others/members/Uladzislava.jpeg?raw=true',
-        bio: 'Lada excels at crafting seamless user interfaces. She has been interested in coding since her school days and continuously strives to expand her knowledge.',
-        contribution: [
-          'As Team Lead, fostered a collaborative project environment from inception to completion.',
-          'Engineered website routing to enhance navigation and user experience.',
-          'Developed a robust component for integrating commercetools SDK, showcasing commitment to project excellence.',
-          'Designed and implemented the «Main» page with an inviting and intuitive user interface.',
-          'Enhanced functionality and accessibility of product information on the product «Catalog» page.',
-        ],
-        github: 'https://github.com/sunlaa',
-        gitName: 'sunlaa',
-      },
-      {
-        name: 'Katsiaryna Stanevich',
-        role: 'Frontend Developer',
-        image: 'https://github.com/sunlaa/commerce-images/blob/main/others/members/Katsiaryna.jpeg?raw=true',
-        bio: 'Katsiaryna is passionate about creating user-friendly and visually appealing web applications. She is improving her coding skills through continuous learning.',
-        contribution: [
-          'Wrote code to load products into commercetools, optimizing data management and retrieval.',
-          'Developed a secure «Login» page to ensure safe access and user authentication.',
-          'Implemented a detailed «Product» description page to improve user experience with comprehensive information.',
-          'Contributed to expanding an SDK integration component, enhancing its functionality.',
-          'Created an engaging and visually appealing «About Us» page with valuable insights into the project team and its overarching mission.',
-        ],
-        github: 'https://github.com/katyastan',
-        gitName: 'katyastan',
-      },
-      {
-        name: 'Pavel Terno',
-        role: 'Frontend Developer',
-        image: 'https://github.com/sunlaa/commerce-images/blob/main/others/members/Pavel.png?raw=true',
-        bio: 'Pavel is an enthusiastic developer with a passion for creating functional websites. He is always eager to learn new technologies and improve his coding skills.',
-        contribution: [
-          'Created a robust validation component ensuring data integrity across modules.',
-          'Implemented an intuitive «Registration» page for seamless user onboarding.',
-          'Crafted a user-friendly «Profile» page to enhance user engagement and personalization.',
-          'Contributed to extending a versatile SDK-compatible component, enhancing platform capabilities.',
-          'Designed and implemented a streamlined shopping «Cart» page for efficient e-commerce transactions.',
-        ],
-        github: 'https://github.com/pahanchickt',
-        gitName: 'pahanchickt',
-      },
-    ];
 
     sectionAbout.appendChildren(aboutTitle);
 
